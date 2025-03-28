4be0b9a5debe79af5e73b0282985a6044a7a2676
Gabo <92454505+gabcrrstt@users.noreply.github.com>
2025-03-28 11:12:14 -0400

    criacao da rota de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CarrosModule } from './carros/carros.module';
 import { CnhModule } from './cnh/cnh.module';
 import { MultasController } from './multas/multas.controller';
 import { MultasModule } from './multas/multas.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CarrosModule, CnhModule, MultasModule],
+  imports: [CarrosModule, CnhModule, MultasModule, UsersModule],
   controllers: [AppController, MultasController],
   providers: [AppService],
 })
