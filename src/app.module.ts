769e75420eb5fba65535b39a826251abc37a2b71
Gabo <92454505+gabcrrstt@users.noreply.github.com>
2025-04-03 22:27:47 -0400

    module e service db criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { DbModule } from './db/db.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
