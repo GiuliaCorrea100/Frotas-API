0c1ab16a35d236c8038d600fe0b11680aa09afb4
Gabo <92454505+gabcrrstt@users.noreply.github.com>
2025-04-03 22:41:52 -0400

    resolvido o problema

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { DbModule } from './db/db.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
