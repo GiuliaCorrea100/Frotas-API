59b90d692ee4013edfd32ddc46da4421ac62bd56
Nataly Tobias <nataly.tobias11@gmail.com>
2025-06-25 10:48:10 -0300

    atualizações

--- src/usersingu/usersingu.controller.ts
+++ src/usersingu/usersingu.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserSinguDto } from './usersingu.dto';
 import { UserSinguService } from './usersingu.service';
 
