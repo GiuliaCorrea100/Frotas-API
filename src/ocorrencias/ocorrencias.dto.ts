98ab7a6d51da75c75d86f769fe1539ac6e4c1c0f
Nataly Tobias <nataly.tobias11@gmail.com>
2025-08-06 23:02:02 -0300

    criação do modulo de ocorrencias

--- src/ocorrencias/ocorrencias.dto.ts
+++ src/ocorrencias/ocorrencias.dto.ts
@@ -2,11 +2,13 @@ export class OcorrenciasDto {
   idOcorrencia?: number;
   descricao: string;
   idCorrida?: number;
+  dataRegistro: Date;
 }
 
 export interface FindAllParameters {
   descricao: string;
   idCorrida: number;
+  dataRegistro: Date;
 }
 
 export class CorridasRouteParameters {
