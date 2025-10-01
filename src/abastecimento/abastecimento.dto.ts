736f3b11c7a84dfefa36b3681ce9649a9c1b8d67
Nataly Tobias <nataly.tobias11@gmail.com>
2025-10-01 12:04:01 -0300

    atts da develop

--- src/abastecimento/abastecimento.dto.ts
+++ src/abastecimento/abastecimento.dto.ts
@@ -1,25 +1,18 @@
 export class AbastecimentoDto {
   idAbastecimento?: number;
-  litros: number;
-  codPagamento: number;
-  precoFinal: number;
-  dataAbastecimento: Date;
-
-  // adicionei as colunas
-  valorUnitario: number;
-  justificativaAlteracao?: string;
-
-  nomeTipoCombustivel?: string;
 
   //chave estrangeira
   idTipoCombustivel?: number;
   idCorrida?: number;
 
   codigoPagamento: string;
-
+  dataAbastecimento: Date;
   quantidade: number;
-
+  valorUnitario: number;
   valorTotal: number;
+  justificativaAlteracao: string;
+
+  nomeTipoCombustivel?: string;
 }
 
 export interface FindAllParameters {
