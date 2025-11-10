aac4c7f79c18865be36c40be342d27522b03fe0c
Giulia Correa <giuliarafaela32@gmail.com>
2025-11-10 10:45:18 -0400

    Quando uma multa é cadastrada,identifica qual motorista estava com o veículo na data da infração

--- src/multa/multa.module.ts
+++ src/multa/multa.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+// INÍCIO DA MODIFICAÇÃO (Importar forwardRef)
+import { Module, forwardRef } from '@nestjs/common';
+// FIM DA MODIFICAÇÃO
 import { MultaService } from './multa.service';
 import { MultaController } from './multa.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MultaEntity } from 'src/db/entities/multa.entity';
 import { LogModule } from 'src/log/log.module';
+// INÍCIO DA MODIFICAÇÃO (Importar CorridaModule)
+import { CorridaModule } from '../corrida/corrida.module';
+// FIM DA MODIFICAÇÃO
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MultaEntity]), LogModule],
+  imports: [
+    TypeOrmModule.forFeature([MultaEntity]),
+    LogModule,
+    // INÍCIO DA MODIFICAÇÃO (Adicionar CorridaModule com forwardRef)
+    // Adicionado para permitir a injeção do CorridaService no MultaService
+    forwardRef(() => CorridaModule),
+    // FIM DA MODIFICAÇÃO
+  ],
   controllers: [MultaController],
   providers: [MultaService],
 })
