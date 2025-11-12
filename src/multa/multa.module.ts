e12cbb14562060b39ab291b9284e9d6e946f0c25
Marco A. Dausen <dausen@unir.br>
2025-11-12 08:15:42 -0400

    fix: adição de de import no multa module

--- src/multa/multa.module.ts
+++ src/multa/multa.module.ts
@@ -4,9 +4,10 @@ import { MultaController } from './multa.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MultaEntity } from 'src/db/entities/multa.entity';
 import { LogModule } from 'src/log/log.module';
+import { CorridaModule } from 'src/corrida/corrida.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MultaEntity]), LogModule],
+  imports: [TypeOrmModule.forFeature([MultaEntity]), CorridaModule, LogModule],
   controllers: [MultaController],
   providers: [MultaService],
 })
