16c3f835b70515c2675e4159054470d943d94b33
Giulia Correa <giuliarafaela32@gmail.com>
2026-04-28 10:07:57 -0400

    ajustado texto nomodal de rejeitar recurso / situacao da multa 'MULTA ANULADA' / campo de justificativa em rejeitar recurso(incompleto)

--- src/multa/multa.service.ts
+++ src/multa/multa.service.ts
@@ -70,7 +70,7 @@ export class MultaService {
 
     const dadosAntigos = { ...multa };
 
-    multa.situacao = 'RECURSO ACEITO - MULTA ANULADA';
+    multa.situacao = 'MULTA ANULADA';
 
     const multaAtualizada = await this.MultaRepository.save(multa);
 
